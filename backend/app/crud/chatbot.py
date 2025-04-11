6800d3cf2be9011005e3e3446948e5e9d36ef5a2
Mohtasim Hossain <mohtasimhossain@Mohtasims-Mac.local>
2025-04-12 03:03:31 +0600

    added/updated crud logic for chatbot and chatbot conversation

--- backend/app/crud/chatbot.py
+++ backend/app/crud/chatbot.py
@@ -7,16 +7,18 @@ class ChatbotCrud():
     def __init__(self):
         self.collection = base_chatbot_collection
         
-    async def create_chatbot(self, chatbot: ChatbotCreate, user_id: str):
+    async def create_chatbot(self, chatbot: ChatbotCreate):
         collection = self.collection
-        chatbot_dict = chatbot.model_dump(mode="json")
-        chatbot_dict["user_id"] = str(user_id)
-        # new_chatbot = await collection.insert_one({**chatbot_dict, "user_id": str(user_id)})
+        chatbot_dict = chatbot.model_dump(mode='json')
         new_chatbot = await collection.insert_one(chatbot_dict)
         return new_chatbot
     
     async def get_chatbot_by_id(self, chatbot_id: str, user_id: str):
-        chatbot = await self.collection.find_one({"_id": ObjectId(chatbot_id), "user_id": user_id})
+        chatbot = await self.collection.find_one({"_id": ObjectId(chatbot_id), "user_id": str(user_id)})
+        return chatbot
+    
+    async def get_chatbot_by_chatbot_name_and_user_id(self, name: str, user_id: str):
+        chatbot = await self.collection.find_one({"name": name, "user_id": user_id})
         return chatbot
     
     async def update_chatbot(self, chatbot_id: str, update_data: ChatbotUpdate):
